83064445153d728656a6ec6d447d44840d8a79bc
dengyueyun <dengyueyun666@sina.com>
2019-09-01 22:07:17 +0800

    Finished Chapter 4 in Ray Tracing in a Weekend.

--- main.cpp
+++ main.cpp
@@ -1,23 +1,56 @@
 #include <fstream>
 #include <iostream>
 
-#include "vec3.h"
+#include "ray.h"
 
 using namespace std;
 
-int main() {
-  int nx = 200;
-  int ny = 100;
-  fstream fs("image.ppm", ios::out);
-  fs << "P3\n" << nx << " " << ny << "\n255\n";
-  for (int j = ny - 1; j >= 0; j--) {
-    for (int i = 0; i < nx; i++) {
-      vec3 col(float(i) / float(nx), float(j) / float(ny), 0.2);
-      int ir = int(255.99 * col[0]);
-      int ig = int(255.99 * col[1]);
-      int ib = int(255.99 * col[2]);
-      fs << ir << " " << ig << " " << ib << "\n";
+bool hit_sphere(const vec3& center, float radius, const ray& r)
+{
+    vec3 oc = r.origin() - center;
+    float a = dot(r.direction(), r.direction());
+    float b = 2.0 * dot(oc, r.direction());
+    float c = dot(oc, oc) - radius * radius;
+    float discriminant = b * b - 4 * a * c;
+    return discriminant > 0;
+}
+
+vec3 color(const ray& r)
+{
+    if (hit_sphere(vec3(0, 0, -1), 0.5, r))
+        return vec3(1.0, 0.0, 0.0);
+    vec3 unit_direction = unit_vector(r.direction());
+    float t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+}
+
+int main(int argc, char** argv)
+{
+    int nx = 200;
+    int ny = 100;
+    string filename;
+    if (argc == 1)
+        filename = "image.ppm";
+    else
+        filename = argv[1];
+    fstream fs(filename, ios::out);
+    fs << "P3\n"
+       << nx << " " << ny << "\n255\n";
+    vec3 lower_left_corner(-2.0, -1.0, -1.0);
+    vec3 horizontal(4.0, 0.0, 0.0);
+    vec3 vertical(0.0, 2.0, 0.0);
+    vec3 origin(0.0, 0.0, 0.0);
+    for (int j = ny - 1; j >= 0; j--) {
+        for (int i = 0; i < nx; i++) {
+            float u = float(i) / float(nx);
+            float v = float(j) / float(ny);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+            vec3 col = color(r);
+            int ir = int(255.99 * col[0]);
+            int ig = int(255.99 * col[1]);
+            int ib = int(255.99 * col[2]);
+            fs << ir << " " << ig << " " << ib << "\n";
+        }
     }
-  }
-  fs.close();
+    fs.close();
 }
