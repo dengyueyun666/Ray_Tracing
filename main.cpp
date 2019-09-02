c535164538e292f28f5641bc57560e5d2ec3ded9
dengyueyun <dengyueyun666@sina.com>
2019-09-02 14:38:47 +0800

    Finished Chapter 7 in Ray Tracing in a Weekend.

--- main.cpp
+++ main.cpp
@@ -8,11 +8,21 @@
 
 using namespace std;
 
+vec3 random_in_unit_sphere()
+{
+    vec3 p;
+    do {
+        p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
+    } while (p.squared_length() >= 1.0);
+    return p;
+}
+
 vec3 color(const ray& r, hitable* world)
 {
     hit_record rec;
-    if (world->hit(r, 0.0, FLT_MAX, rec)) {
-        return 0.5 * vec3(rec.normal.x() + 1, rec.normal.y() + 1, rec.normal.z() + 1);
+    if (world->hit(r, 0.001, FLT_MAX, rec)) {
+        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
+        return 0.5 * color(ray(rec.p, target - rec.p), world);
     } else {
         vec3 unit_direction = unit_vector(r.direction());
         float t = 0.5 * (unit_direction.y() + 1.0);
@@ -45,10 +55,12 @@ int main(int argc, char** argv)
                 float u = float(i + drand48()) / float(nx);
                 float v = float(j + drand48()) / float(ny);
                 ray r = cam.get_ray(u, v);
-                vec3 p = r.point_at_parameter(2.0);
+                //vec3 p = r.point_at_parameter(2.0);
                 col += color(r, world);
             }
             col /= float(ns);
+            // Gamma Correction
+            col = vec3(sqrt(col[0]),sqrt(col[1]),sqrt(col[2]));
             int ir = int(255.99 * col[0]);
             int ig = int(255.99 * col[1]);
             int ib = int(255.99 * col[2]);
