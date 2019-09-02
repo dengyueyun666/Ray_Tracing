5d65058e485f05328614d9e445739a817686bc8b
dengyueyun <dengyueyun666@sina.com>
2019-09-02 16:39:02 +0800

    Finished Chapter 8 in Ray Tracing in a Weekend.

--- main.cpp
+++ main.cpp
@@ -4,25 +4,22 @@
 #include "camera.h"
 #include "float.h"
 #include "hitable_list.h"
+#include "material.h"
 #include "sphere.h"
 
 using namespace std;
 
-vec3 random_in_unit_sphere()
-{
-    vec3 p;
-    do {
-        p = 2.0 * vec3(drand48(), drand48(), drand48()) - vec3(1, 1, 1);
-    } while (p.squared_length() >= 1.0);
-    return p;
-}
-
-vec3 color(const ray& r, hitable* world)
+vec3 color(const ray& r, hitable* world, int depth)
 {
     hit_record rec;
     if (world->hit(r, 0.001, FLT_MAX, rec)) {
-        vec3 target = rec.p + rec.normal + random_in_unit_sphere();
-        return 0.5 * color(ray(rec.p, target - rec.p), world);
+        ray scattered;
+        vec3 attenuation;
+        if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
+            return attenuation * color(scattered, world, depth + 1);
+        } else {
+            return vec3(0, 0, 0);
+        }
     } else {
         vec3 unit_direction = unit_vector(r.direction());
         float t = 0.5 * (unit_direction.y() + 1.0);
@@ -43,10 +40,12 @@ int main(int argc, char** argv)
     fstream fs(filename.c_str(), ios::out);
     fs << "P3\n"
        << nx << " " << ny << "\n255\n";
-    hitable* list[2];
-    list[0] = new sphere(vec3(0, 0, -1), 0.5);
-    list[1] = new sphere(vec3(0, -100.5, -1), 100);
-    hitable* world = new hitable_list(list, 2);
+    hitable* list[4];
+    list[0] = new sphere(vec3(0, 0, -1), 0.5, new lambertian(vec3(0.8, 0.3, 0.3)));
+    list[1] = new sphere(vec3(0, -100.5, -1), 100, new lambertian(vec3(0.8, 0.8, 0.0)));
+    list[2] = new sphere(vec3(1, 0, -1), 0.5, new metal(vec3(0.8, 0.6, 0.2), 1.0));
+    list[3] = new sphere(vec3(-1, 0, -1), 0.5, new metal(vec3(0.8, 0.8, 0.8), 0.3));
+    hitable* world = new hitable_list(list, 4);
     camera cam;
     for (int j = ny - 1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
@@ -56,11 +55,11 @@ int main(int argc, char** argv)
                 float v = float(j + drand48()) / float(ny);
                 ray r = cam.get_ray(u, v);
                 //vec3 p = r.point_at_parameter(2.0);
-                col += color(r, world);
+                col += color(r, world, 0);
             }
             col /= float(ns);
             // Gamma Correction
-            col = vec3(sqrt(col[0]),sqrt(col[1]),sqrt(col[2]));
+            col = vec3(sqrt(col[0]), sqrt(col[1]), sqrt(col[2]));
             int ir = int(255.99 * col[0]);
             int ig = int(255.99 * col[1]);
             int ib = int(255.99 * col[2]);
