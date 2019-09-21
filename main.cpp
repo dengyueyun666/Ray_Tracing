b9adbb37b79555c536db9781b26e71e194483a72
dengyueyun <dengyueyun666@sina.com>
2019-09-21 11:20:16 +0800

    Finished Chapter 6 in Ray Tracing in the Next Week.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 
+#include "aarect.h"
 #include "bvh.h"
 #include "camera.h"
 #include "float.h"
@@ -22,15 +23,14 @@ vec3 color(const ray& r, hitable* world, int depth)
     if (world->hit(r, 0.001, FLT_MAX, rec)) {
         ray scattered;
         vec3 attenuation;
+        vec3 emitted = rec.mat_ptr->emitted(rec.u, rec.v, rec.p);
         if (depth < 50 && rec.mat_ptr->scatter(r, rec, attenuation, scattered)) {
-            return attenuation * color(scattered, world, depth + 1);
+            return emitted + attenuation * color(scattered, world, depth + 1);
         } else {
-            return vec3(0, 0, 0);
+            return emitted;
         }
     } else {
-        vec3 unit_direction = unit_vector(r.direction());
-        float t = 0.5 * (unit_direction.y() + 1.0);
-        return (1.0 - t) * vec3(1.0, 1.0, 1.0) + t * vec3(0.5, 0.7, 1.0);
+        return vec3(0, 0, 0);
     }
 }
 
@@ -59,6 +59,34 @@ hitable* earth()
     unsigned char* tex_data = stbi_load("earthmap.jpg", &nx, &ny, &nn, 0);
     material* mat = new lambertian(new image_texture(tex_data, nx, ny));
     return new sphere(vec3(0, 0, 0), 2, mat);
+}
+
+hitable* simple_light()
+{
+    texture* pertext = new noise_texture(4);
+    hitable** list = new hitable*[4];
+    list[0] = new sphere(vec3(0, -1000, 0), 1000, new lambertian(pertext));
+    list[1] = new sphere(vec3(0, 2, 0), 2, new lambertian(pertext));
+    list[2] = new sphere(vec3(0, 7, 0), 2, new diffuse_light(new constant_texture(vec3(4, 4, 4))));
+    list[3] = new xy_rect(3, 5, 1, 3, -2, new diffuse_light(new constant_texture(vec3(4, 4, 4))));
+    return new hitable_list(list, 4);
+}
+
+hitable* cornell_box()
+{
+    hitable** list = new hitable*[6];
+    int i = 0;
+    material* red = new lambertian(new constant_texture(vec3(0.65, 0.05, 0.05)));
+    material* white = new lambertian(new constant_texture(vec3(0.73, 0.73, 0.73)));
+    material* green = new lambertian(new constant_texture(vec3(0.12, 0.45, 0.15)));
+    material* light = new diffuse_light(new constant_texture(vec3(15, 15, 15)));
+    list[i++] = new flip_normals(new yz_rect(0, 555, 0, 555, 555, green));
+    list[i++] = new yz_rect(0, 555, 0, 555, 0, red);
+    list[i++] = new xz_rect(213, 343, 227, 332, 554, light);
+    list[i++] = new flip_normals(new xz_rect(0, 555, 0, 555, 555, white));
+    list[i++] = new xz_rect(0, 555, 0, 555, 0, white);
+    list[i++] = new flip_normals(new xy_rect(0, 555, 0, 555, 555, white));
+    return new hitable_list(list, i);
 }
 
 hitable* random_scene()
@@ -94,8 +122,8 @@ hitable* random_scene()
 
 int main(int argc, char** argv)
 {
-    int nx = 400;
-    int ny = 200;
+    int nx = 800;
+    int ny = 800;
     int ns = 100;
     string filename;
     if (argc == 1)
@@ -108,14 +136,19 @@ int main(int argc, char** argv)
     // hitable* world = random_scene();
     // hitable* world = two_sphere();
     // hitable* world = two_perlin_sphere();
-    hitable* world = earth();
+    // hitable* world = earth();
+    // hitable* world = simple_light();
+    hitable* world = cornell_box();
 
-    vec3 lookfrom(13, 2, 3);
-    vec3 lookat(0, 0, 0);
+    // vec3 lookfrom(13, 2, 3);
+    // vec3 lookat(0, 3, 0);
+    vec3 lookfrom(278, 278, -800);
+    vec3 lookat(278, 278, 0);
     float dist_to_focus = 10.0;
     float aperture = 0.0;
+    float vfov = 40.0;
 
-    camera cam(lookfrom, lookat, vec3(0, 1, 0), 20, float(nx) / float(ny), aperture, dist_to_focus, 0.0, 1.0);
+    camera cam(lookfrom, lookat, vec3(0, 1, 0), vfov, float(nx) / float(ny), aperture, dist_to_focus, 0.0, 1.0);
 
     for (int j = ny - 1; j >= 0; j--) {
         for (int i = 0; i < nx; i++) {
