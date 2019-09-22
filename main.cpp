4a43969001b698fd1dcf5b881746982308ba5e52
dengyueyun <dengyueyun666@sina.com>
2019-09-22 09:28:53 +0800

    Finished Chapter 7 in Ray Tracing in the Next Week.

--- main.cpp
+++ main.cpp
@@ -4,6 +4,7 @@
 #define STB_IMAGE_IMPLEMENTATION
 
 #include "aarect.h"
+#include "box.h"
 #include "bvh.h"
 #include "camera.h"
 #include "float.h"
@@ -74,7 +75,7 @@ hitable* simple_light()
 
 hitable* cornell_box()
 {
-    hitable** list = new hitable*[6];
+    hitable** list = new hitable*[8];
     int i = 0;
     material* red = new lambertian(new constant_texture(vec3(0.65, 0.05, 0.05)));
     material* white = new lambertian(new constant_texture(vec3(0.73, 0.73, 0.73)));
@@ -86,6 +87,8 @@ hitable* cornell_box()
     list[i++] = new flip_normals(new xz_rect(0, 555, 0, 555, 555, white));
     list[i++] = new xz_rect(0, 555, 0, 555, 0, white);
     list[i++] = new flip_normals(new xy_rect(0, 555, 0, 555, 555, white));
+    list[i++] = new translate(new rotate_y(new box(vec3(0, 0, 0), vec3(165, 165, 165), white), -18), vec3(130, 0, 65));
+    list[i++] = new translate(new rotate_y(new box(vec3(0, 0, 0), vec3(165, 330, 165), white), 15), vec3(265, 0, 295));
     return new hitable_list(list, i);
 }
 
