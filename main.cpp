3560d0c1ee867e5135388fb41ffec72f3b577048
dengyueyun <dengyueyun666@sina.com>
2019-09-22 10:10:46 +0800

    Finished Chapter 8 in Ray Tracing in the Next Week.

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "box.h"
 #include "bvh.h"
 #include "camera.h"
+#include "constant_medium.h"
 #include "float.h"
 #include "hitable_list.h"
 #include "material.h"
@@ -92,6 +93,27 @@ hitable* cornell_box()
     return new hitable_list(list, i);
 }
 
+hitable* cornell_smoke()
+{
+    hitable** list = new hitable*[8];
+    int i = 0;
+    material* red = new lambertian(new constant_texture(vec3(0.65, 0.05, 0.05)));
+    material* white = new lambertian(new constant_texture(vec3(0.73, 0.73, 0.73)));
+    material* green = new lambertian(new constant_texture(vec3(0.12, 0.45, 0.15)));
+    material* light = new diffuse_light(new constant_texture(vec3(7, 7, 7)));
+    list[i++] = new flip_normals(new yz_rect(0, 555, 0, 555, 555, green));
+    list[i++] = new yz_rect(0, 555, 0, 555, 0, red);
+    list[i++] = new xz_rect(113, 443, 127, 432, 554, light);
+    list[i++] = new flip_normals(new xz_rect(0, 555, 0, 555, 555, white));
+    list[i++] = new xz_rect(0, 555, 0, 555, 0, white);
+    list[i++] = new flip_normals(new xy_rect(0, 555, 0, 555, 555, white));
+    hitable* b1 = new translate(new rotate_y(new box(vec3(0, 0, 0), vec3(165, 165, 165), white), -18), vec3(130, 0, 65));
+    hitable* b2 = new translate(new rotate_y(new box(vec3(0, 0, 0), vec3(165, 330, 165), white), 15), vec3(265, 0, 295));
+    list[i++] = new constant_medium(b1, 0.01, new constant_texture(vec3(1.0, 1.0, 1.0)));
+    list[i++] = new constant_medium(b2, 0.01, new constant_texture(vec3(0.0, 0.0, 0.0)));
+    return new hitable_list(list, i);
+}
+
 hitable* random_scene()
 {
     int n = 500;
@@ -141,7 +163,8 @@ int main(int argc, char** argv)
     // hitable* world = two_perlin_sphere();
     // hitable* world = earth();
     // hitable* world = simple_light();
-    hitable* world = cornell_box();
+    // hitable* world = cornell_box();
+    hitable* world = cornell_smoke();
 
     // vec3 lookfrom(13, 2, 3);
     // vec3 lookat(0, 3, 0);
